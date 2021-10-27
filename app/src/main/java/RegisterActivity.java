6e646123624c589c74922f27883608babbc0db1c
maria-jpg <maria.p1527@gmail.com>
2021-10-26 20:01:04 -0700

    Updating main branch

---
+++ app/src/main/java/RegisterActivity.java
@@ -0,0 +1,10 @@
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
