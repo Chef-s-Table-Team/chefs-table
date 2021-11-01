98d4e5ac37019e7486eda673af4cd53e5e0ba95f
maria-jpg <maria.p1527@gmail.com>
2021-11-01 01:37:16 -0700

    Updated branch to current

--- app/src/main/java/com/example/chefstable/LoginActivity.java
+++ app/src/main/java/com/example/chefstable/LoginActivity.java
@@ -39,137 +39,146 @@ public class LoginActivity extends AppCompatActivity {
 
 
         if (ParseUser.getCurrentUser() != null) {
-    /*
-        if (ParseUser.getCurentUser() != null) {
 
-            go_to_main_activity();
+                go_to_main_activity();
 
         }
-*/
-        etUsername = findViewById(R.id.etUsername);
 
-        etPassword = findViewById(R.id.etPassword);
 
-        login_button = findViewById(R.id.login_button);
 
-        sign_up_button = findViewById(R.id.sign_up_button);
+            etUsername = findViewById(R.id.etUsername);
 
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            etPassword = findViewById(R.id.etPassword);
 
-                //log in message for error logging
+            login_button = findViewById(R.id.login_button);
 
-                Log.i(TAG, "onClick login btnCaptureImage");
+            sign_up_button = findViewById(R.id.sign_up_button);
 
-                //get text from username + password fields, and login user
+            login_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-                String user = etUsername.getText().toString();
+                    //log in message for error logging
 
-                String password = etPassword.getText().toString();
+                    Log.i(TAG, "onClick login");
 
-                log_in_user(user, password);
+                    //get text from username + password fields, and login user
 
-            }
-        });
+                    String user = etUsername.getText().toString();
 
-        sign_up_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "onClick sign up button");
+                    String password = etPassword.getText().toString();
 
-                //get text from username + password fields, and sign up user
 
-                String user = etUsername.getText().toString();
+                    log_in_user(user, password);
 
-                String password = etPassword.getText().toString();
+                }
+            });
 
-                create_user(user, password);
+            sign_up_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i(TAG, "onClick sign up button");
 
-            }
-        });
+                    //get text from username + password fields, and sign up user
 
-    }
+                    Intent b = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(b);
 
+
+                }
+            });
+
+        }
+        /*
+        private void create_user (String new_user, String password){
+
+            //create parse user
+
+<<<<<<<<< Temporary merge branch 1
+    /*
     private void create_user(String new_user, String password) {
 
         //create parse user
-        /*
+
         ParseUser user = new ParseUser(); //parse app code to be written
+=========
+            ParseUser user = new ParseUser(); //parse app code to be written
 
-        //set user + password for User class instance
+            //set user + password for User class instance
+>>>>>>>>> Temporary merge branch 2
 
-        user.setUsername(new_user);
+            user.setUsername(new_user);
 
-        user.setPassword(password);
+            user.setPassword(password);
 
-        //create sign up to be done in background
+            //create sign up to be done in background
 
-        user.signUpInBackground(new SignUpCallback() {
+            user.signUpInBackground(new SignUpCallback() {
 
-            public void done(ParseException e) {
+                public void done(ParseException e) {
 
-                if (e == null) { //log in valid?
+                    if (e == null) { //log in valid?
+
+                        go_to_main_activity();
+
+                        Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+
+                        //let user use app afterwards :)
+
+                    } else { //issue with sign up
+
+                        Log.e(TAG, "Sign up failed, error: ", e);
+
+                        Toast.makeText(LoginActivity.this, "Sign up issue.", Toast.LENGTH_SHORT).show();
+
+                        return;
+
+                    }
+
+                }
+
+            });
+*/
+
+        private void log_in_user (String user, String password){
+
+            Log.i(TAG, "Attempting to log in user: " + user + "...");
+
+        //user signed in properly? go to main activity
+            ParseUser.logInInBackground(user, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+
+                    if (e != null) {
+
+                        Log.e(TAG, "Log in issue: ", e);
+
+                        Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
+
+                        return;
+
+                    }
 
                     go_to_main_activity();
 
                     Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
 
-                    //let user use app afterwards :)
-
-                } else { //issue with sign up
-
-                    Log.e(TAG, "Sign up failed, error: ", e);
-
-                    Toast.makeText(LoginActivity.this, "Sign up issue.", Toast.LENGTH_SHORT).show();
-
-                    return;
-
                 }
 
-            }
+            });
 
-        });
-    */
-    }
 
-    private void log_in_user(String user, String password) {
-
-        Log.i(TAG, "Attempting to log in user: " + user + "...");
-
-        //user signed in properly? go to main activity
-        /*
-        ParseUser.logInInBackground(user, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-
-                if (e != null) {
-
-                    Log.e(TAG, "Log in issue: ", e);
-
-                    Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
-
-                    return;
-
-                }
-
-                go_to_main_activity();
-
-                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
-
-            }
-
-        });
-
-        */
-    }
-
-    private void go_to_main_activity() {
-
-        Intent i = new Intent(this, MainActivity.class);
-
-        startActivity(i);
 
     }
 
-}
+
+        private void go_to_main_activity () {
+
+            Intent i = new Intent(this, MainActivity.class);
+
+            startActivity(i);
+            finish();
+
+        }
+
+    }
